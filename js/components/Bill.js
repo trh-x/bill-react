c153adfe380ae86606443e07e7ae906e802b15e0
Tom Rees-Herdman <tom@techanvil.co.uk>
2015-09-20 07:22:19 +0100

    Adding files

---
+++ js/components/Bill.js
@@ -0,0 +1,11 @@
+import Statement from './Bill/Statement';
+import Package from './Bill/Package';
+import CallCharges from './Bill/CallCharges';
+import OnlineStore from './Bill/OnlineStore';
+
+export default {
+    Statement,
+    Package,
+    CallCharges,
+    OnlineStore
+};
