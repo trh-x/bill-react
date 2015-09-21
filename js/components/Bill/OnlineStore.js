f32d09775d3f33707855c1d037779935c9a91801
Tom Rees-Herdman <treesherdman@bodog.co.uk>
2015-09-21 20:11:24 +0100

    Rename onlineStore

--- js/components/Bill/OnlineStore.js
+++ js/components/Bill/OnlineStore.js
@@ -7,19 +7,19 @@ class OnlineStore extends React.Component {
     render() {
         let children = [];
 
-        if (this.props.bill.onlineStore != null) {
+        if (this.props.bill.skyStore != null) {
             function renderItems(items) {
                 return items.map( (item, index) => <Item key={index} name={item.title} value={CurrencyUtils.formatCurrency(item.cost)} /> );
             }
 
             children = [
                 <SubSection key="rentals" title="Rentals">
-                    { renderItems(this.props.bill.onlineStore.rentals) }
+                    { renderItems(this.props.bill.skyStore.rentals) }
                 </SubSection>,
                 <SubSection key="buyAndKeep" title="Buy and Keep">
-                    { renderItems(this.props.bill.onlineStore.buyAndKeep) }
+                    { renderItems(this.props.bill.skyStore.buyAndKeep) }
                 </SubSection>,
-                <Total key="total" total={this.props.bill.onlineStore.total} />
+                <Total key="total" total={this.props.bill.skyStore.total} />
             ];
         }
 
