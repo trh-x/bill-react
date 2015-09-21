e14a19d85e24cdd1d9483819b682cedaffbc6ad1
Tom Rees-Herdman <tom.r.herdman@gmail.com>
2015-09-21 23:34:17 +0100

    Move sample data to folder

--- js/testUtils/BillTestUtils.js
+++ js/testUtils/BillTestUtils.js
@@ -3,6 +3,6 @@ import fs from 'fs';
 export default {
     mockBill() {
         // For some reason simply requiring the json file wasn't parsing arrays properly, leaving them empty!
-        return JSON.parse(fs.readFileSync('shortBill.json'));
+        return JSON.parse(fs.readFileSync('data/shortBill.json'));
     }
 }
